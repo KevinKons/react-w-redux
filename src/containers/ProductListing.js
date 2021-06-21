f90427a330f8e40ba150e9d8e32079bb6021bbd6
kevinkons <kevin.kons@hotmail.com>
2021-06-21 12:05:10 -0300

    .

---
+++ src/containers/ProductListing.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchProducts } from '../redux/actions/productActions'
+
+import ProductComponent from './ProductComponent'
+
+const ProductListing = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch])
+
+  return (
+    <div className='ui grid container'>
+      <ProductComponent />
+    </div>
+  );
+}
+
+export default ProductListing;
